b4ab423c13a7dd42c76088b3bb6f2e1594a48b7d
chimpoka <artem.voronov90@gmail.com>
2018-11-13 22:07:35 +0300

    Create new project. Create Field object and script.

---
+++ Assets/Scripts/Field.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Field : MonoBehaviour
+{
+    public int width = 10;
+    public int height = 10;
+
+	void Start ()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x = (float)width / 10;
+        scale.z = (float)height / 10;
+        transform.localScale = scale;
+
+        Vector2 textureScale = new Vector2(width, height);
+        GetComponent<Renderer>().material.mainTextureScale = textureScale;
+    }
+	
+
+	void Update ()
+    {
+		
+	}
+}
